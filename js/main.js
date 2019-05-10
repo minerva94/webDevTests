d0bf8f780b32787e8c48133c17737046df1fd8eb
Giulia Massa <giulia.massa.sun@gmail.com>
2019-05-10 19:06:34 +0100

    Finished js introduction. Learned ho to create a function and how to let HTML and js communicate between each other.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,66 @@
 
-var welcome_message = "This is welcome message stored inside a variable";
-var myAge = 24;
-var exactAge =24.7;
+/*var ifPremiumUser = true;
+
+if(ifPremiumUser == true){
+    console.log("Thanks for being a loyal customer!")
+}else{
+    console.log("You should subscribe to our premium service.")
+}
+
+var myAge =24;
+
+if(myAge <1) {
+    console.log ("You are a baby.")
+}else if (myAge <3){
+    console.log ("You are a toddler.")
+}else if (myAge <10){
+    console.log("You are a big kid")
+}else if (myAge <=19){
+    console.log("You are a teenager")
+}else if (myAge <40){
+    console.log("You are an adult.")
+}else{
+    console.log("You must be old, you are not in our records.")
+}
 
 
-alert(welcome_message);
+for(var i=0; i<10; i++){
+    console.log("Current i value is", i);
+}
 
-console.log("This is a log!");
+function printName(name, age){
+    console.log("Hello,", name,". You are", age);
+}
+
+printName("Giulia", "24 years old");
+printName("Marco", "27 years old");*/
+
+var btn= document.getElementById("go-button");
+
+function buttonClicked(){
+    console.log("Button clicked");
+    btn.removeEventListener("click", buttonClicked);
+    document.getElementById("text").innerHTML= "Don't do it!";
+    console.log (document.getElementById("text"));
+}
+
+btn.addEventListener("click", buttonClicked);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
