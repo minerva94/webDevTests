0d16004e94457932e087c23d9cd5fa3fec335c1d
Giulia Massa <giulia.massa.sun@gmail.com>
2019-05-23 12:09:53 +0100

    Variables and textareas

--- js/main.js
+++ js/main.js
@@ -40,11 +40,41 @@ var btn= document.getElementById("go-button");
 function buttonClicked(){
     console.log("Button clicked");
     btn.removeEventListener("click", buttonClicked);
-    document.getElementById("text").innerHTML= "Don't do it!";
-    console.log (document.getElementById("text"));
+
+    var customText = document.getElementsByClassName("my-input");
+    var textArea =document.getElementsByClassName("my-textarea");
+
+    var result = document.getElementById("text");
+
+    result.innerHTML = "Hello, " + customText[1].value + "<br />";
+    result.innerHTML += "Message: " + textArea [0].value;
 }
 
 btn.addEventListener("click", buttonClicked);
+
+/*var hobbies= ["pizza", "gamin", "programming", "music", "reading"];
+
+console.log("I no longer enjoy", hobbies.pop());
+hobbies.push("archery");
+console.log(hobbies.shift());
+hobbies.unshift("blacksmithing");
+
+
+hobbies.forEach(function(item, index){
+    console.log("I like", item, index);
+});
+
+if(hobbies.indexOf("programming")>-1){
+    console.log("I like programming!")
+};
+if(hobbies.indexOf("sports")> -1){
+
+}else{
+    console.log("not in array")
+};*/
+
+
+
 
 
 
